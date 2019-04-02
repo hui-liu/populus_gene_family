08bc28081869da0da8aa07b0c00811815861bcc9
Hui Liu <liuhui.bfu@gmail.com>
2019-04-02 16:43:09 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -29,3 +29,42 @@ awk '{print "wget -e robots=off -r -p -np -k https://treegenesdb.org"$1,"--no-ch
 mv treegenesdb.org/FTP/Genomes/Pita/v2.01/* .
 rm -r treegenesdb.org/
 rm index.html
+
+#piabi
+wget ftp://plantgenie.org/Data/ConGenIE/Picea_abies/v1.0/
+sed 's/href=/\n/g' index.html |grep -E "FASTA|GFF3|BED|Annotation|gtf"| sed 's/">/\t/;s/"//' | cut -f 1 | \
+awk '{print "wget -e robots=off -r -p -np -k", $1, "--no-check-certificate --wait 1"}'| sh
+
+mv plantgenie.org/Data/ConGenIE/Picea_abies/v1.0/* .
+rm -r plantgenie.org/
+rm index.html
+
+# potra
+wget ftp://plantgenie.org/Data/PopGenIE/Populus_tremula/v1.1/
+sed 's/href=/\n/g' index.html |grep -E "fasta|gff3|annotation" | sed 's/">/\t/;s/"//' | cut -f 1 | \
+awk '{print "wget -e robots=off -r -p -np -k", $1, "--no-check-certificate --wait 1"}'| sh
+
+mv plantgenie.org/Data/PopGenIE/Populus_tremula/v1.1/* .
+rm -r plantgenie.org/
+rm index.html
+
+# potrs
+wget ftp://plantgenie.org/Data/PopGenIE/Populus_tremuloides/v1.1/
+sed 's/href=/\n/g' index.html |grep -E "FASTA|GFF3|Annotation"| sed 's/">/\t/;s/"//' | cut -f 1 | \
+awk '{print "wget -e robots=off -r -p -np -k", $1, "--no-check-certificate --wait 1"}'| sh
+
+mv plantgenie.org/Data/PopGenIE/Populus_tremuloides/v1.1/* .
+rm -r plantgenie.org/
+rm index.html
+
+
+
+
+
+
+
+
+
+
+
+
